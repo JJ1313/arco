8bac948f732b849e6cd0d1724d717a353fa46d56
JJ <hgbeckerss@gmail.com>
2025-03-04 14:22:34 -0300

    ADD: Navbar mobile

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-document.getElementById("year").innerText = new Date().getFullYear();
+document.getElementById('year').innerText = new Date().getFullYear();
+
+// ========= NAVBAR CONTROL =========
+const burger = document.getElementById('burger');
+burger.addEventListener('click', (event) => {
+    const menu = document.getElementById(event.target.getAttribute('data-target'));
+    event.target.toggleAttribute('data-active');
+    menu.toggleAttribute('data-visible');
+});
+
+
