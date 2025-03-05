bb23aa1c5a65bc8455688eb9d5efe14deb6cdb7a
JJ <hgbeckerss@gmail.com>
2025-03-05 15:31:03 -0300

    Mobile

--- js/index.js
+++ js/index.js
@@ -8,19 +8,26 @@ burger.addEventListener('click', (event) => {
     menu.toggleAttribute('data-visible');
 });
 
+// ========= COLLAPSIBLE =========
+const btnsCollapsible = document.querySelectorAll('.btn-collapsible');
+btnsCollapsible.forEach((btn) => btn.addEventListener('click', (event) => {
+    const target = event.target.getAttribute('data-target');
+    document.getElementById(target).toggleAttribute('data-visible');
+    event.target.toggleAttribute('data-active');
+}));
 
 // ========= ANIMATION SLIDE IN =========
-document.addEventListener("DOMContentLoaded", function () {
-    const listItems = document.querySelectorAll("#section-services ul li");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const listItems = document.querySelectorAll("#section-services ul li");
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("slide-in");
-            }
-        });
-    }, { threshold: 0.5 }); 
-    listItems.forEach((item) => {
-        observer.observe(item); 
-    });
-});
+//     const observer = new IntersectionObserver((entries) => {
+//         entries.forEach((entry) => {
+//             if (entry.isIntersecting) {
+//                 entry.target.classList.add("slide-in");
+//             }
+//         });
+//     }, { threshold: 0.5 }); 
+//     listItems.forEach((item) => {
+//         observer.observe(item); 
+//     });
+// });
